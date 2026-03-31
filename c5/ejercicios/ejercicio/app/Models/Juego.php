0b207b8a831a9a5b5d3cf6de56a0a5e82300f731
OscarLP09 <oscar.s352656@cesurformacion.com>
2026-03-31 17:20:48 +0200

    ejercicio en curso

---
+++ c5/ejercicios/ejercicio/app/Models/Juego.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Juego extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'release_year',
+        'tags',
+    ];
+
+    protected $casts = [
+        'tags' => 'array',
+    ];
+
+    public function tags() {
+        $tagIds = json_decode(
+            $this->attributes['tags'] ?? '[]', true
+        );
+        return Tag::whereIn('_id', is_array($tagIds) ? $tagIds : [])->get();
+    }
+}
